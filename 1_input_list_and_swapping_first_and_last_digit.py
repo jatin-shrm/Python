4bcfb9bb94d1b100ccbe928ea01f296f7b58da71
jsharma3182002 <91322311+jsharma3182002@users.noreply.github.com>
2021-09-24 17:05:06 +0530

    Add files via upload
    Swapping the first and last digits of pthon list

---
+++ 1_input_list_and_swapping_first_and_last_digit.py
@@ -0,0 +1,11 @@
+n=input("Enter the  elements you want in list separated by space\n")
+Input_list=n.split()
+print("List befor swapping the first and last digits: ",Input_list)
+size=len(Input_list)
+print(size)
+def function1(Input_list):
+    temp=Input_list[0]
+    Input_list[0]=Input_list[size-1]
+    Input_list[size-1]=temp
+    return Input_list
+print(function1(Input_list))
