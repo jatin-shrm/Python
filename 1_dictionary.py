65fb35d14752b0f3ca23a930f6350166603e1c12
Jatin Sharma <91322311+jsharma3182002@users.noreply.github.com>
2021-09-28 20:49:07 +0530

    Dictionary of four words

---
+++ 1_dictionary.py
@@ -0,0 +1,9 @@
+"""Dictionaory of four words
+Words are
+       Eminent, Emerge, Emblem and Embed
+Taken: User only give input of the words which are given in the dictionary"""
+d1={"Eminent":"Famous and Impornat","Emerge":"To appear or come out from somewhere",
+    "Emblem":"An object or symbol that represent something",
+    "Embed":"To fix something firlmly and deeply"}
+p=input("Enter the word \n",)
+print(d1[p])
