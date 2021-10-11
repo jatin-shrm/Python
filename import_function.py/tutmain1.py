56ae092825a174cbbf83131463f23e081ff80abe
Jatin Sharma <91322311+jsharma3182002@users.noreply.github.com>
2021-10-11 11:26:33 +0530

    Create tutmain1.py

---
+++ import_function.py/tutmain1.py
@@ -0,0 +1,8 @@
+def printjeet(string):
+    return f"This my string {string}"
+def add(num1,num2):
+    return num1 + num2 + 6
+if __name__=='__main__':
+    print(printjeet("Jatin"))
+    o=add(4,6)
+    print(o)
