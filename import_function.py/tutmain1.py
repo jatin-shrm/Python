a748a63978292100e617790448dc5418b0848e4f
Jatin Sharma <91322311+jsharma3182002@users.noreply.github.com>
2021-10-11 11:31:29 +0530

    Update tutmain1.py

--- import_function.py/tutmain1.py
+++ import_function.py/tutmain1.py
@@ -2,6 +2,7 @@ def printjeet(string):
     return f"This my string {string}"
 def add(num1,num2):
     return num1 + num2 + 6
+#This if__name=='__main__' function implies that : if we import this file, then the code in this function have will not affect the new file, only the code exceptthis fuction is executable
 if __name__=='__main__':
     print(printjeet("Jatin"))
     o=add(4,6)
