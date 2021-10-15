13ccb9f275e38a3d7d957074b377f27dc5c8bc09
Jatin Sharma <91322311+jsharma3182002@users.noreply.github.com>
2021-10-15 22:44:21 +0530

    Taking another function as its arguments

---
+++ 1_decorators.py
@@ -0,0 +1,10 @@
+def dec1(func1):
+    def nowjatin():
+        print("Executing now")
+        func1()
+        print("Executed")
+    return nowjatin
+@dec1
+def I_am_Jatin():
+    print("I am Jatin")
+I_am_Jatin()
