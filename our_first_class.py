eac7bf3e9a0404ce33632711aae03a47b1bc0202
Jatin Sharma <91322311+jsharma3182002@users.noreply.github.com>
2021-10-17 19:54:27 +0530

    creating our first class

---
+++ our_first_class.py
@@ -0,0 +1,12 @@
+class student:
+    pass
+harry=student()
+larry=student()
+harry.name="Harry"
+harry.std=5
+harry.section=2
+larry.name="Larry"
+larry.std=4
+larry.section=1
+larry.subject=["Math","Physics"]
+print(harry.section,larry.subject)
