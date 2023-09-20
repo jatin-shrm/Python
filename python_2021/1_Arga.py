4ea38bb0764537805e0927cadf0084e3aebedbd3
Jatin <jsharma3182002>
2023-09-20 13:21:15 +0530

    fix: arga

--- python_2021/1_Arga.py
+++
@@ -1,7 +0,0 @@
-def args(another,*args):
-    print(another)
-    for i in args:
-        print(i)
-list1=["Jatin","Parth","Anirudh","Aditya","Soheb"]
-another="Here another is a normal argument"
-args(another,*list1)
