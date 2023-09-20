35b4dc2d55b5479ab4248841c2439e6baf465819
Jatin Sharma <91322311+jsharma3182002@users.noreply.github.com>
2023-09-20 14:04:45 +0530

    Delete 1_Arga.py

--- 1_Arga.py
+++
@@ -1,7 +0,0 @@
-def args(another,*args):
-    print(another)
-    for j in args:
-        print(j)
-list1=["Jatin","Parth","Anirudh","Aditya","Soheb"]
-another="Here another is a normal argument"
-args(another,*list1)
