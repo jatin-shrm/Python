9a88ed5de387a010784c7814f10d1896d704dcf0
Jatin Sharma <jsharma3182002@gmail.com>
2023-10-16 08:44:16 +0530

    d1

---
+++ dictionary/d1.py
@@ -0,0 +1,11 @@
+#Convert two lists into a dictionary
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+thisdict=dict(zip(keys,values))
+# for i in range(len(keys)):
+#     thisdict.update({keys[i]: values[i]})
+# print(thisdict)
+
+#by zip module
+
+print(thisdict)
